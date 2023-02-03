c149fbb9f183cdfef26aa488c387814f6c23daff
Willi4mL <wi.lindbolm@gmail.com>
2023-02-03 13:15:32 +0100

    no changes

--- figure.js
+++ figure.js
@@ -1,7 +1,7 @@
 import { randomize } from "./functions.js"
 
 let correct = document.querySelector('.correct_letters')
-let test = document.querySelector('body')
+let main = document.querySelector('body')
 
 let hangman = {
 	scaffold: document.querySelector('#scaffold'),
@@ -25,32 +25,38 @@ body.style.display = invisible
 arms.style.display = invisible
 legs.style.display = invisible
 
+let randomWord = randomize()
+
 // Create underline
-for (let lines = 0; lines <= randomize().length; lines++) {
+for (let lines = 0; lines <= randomWord.length; lines++) {
 
 	const underLine = document.createElement('div')
 	underLine.style.width = "4em";
 	underLine.style.height = "0.3em";
 	underLine.style.background = "#000000";
-	underLine.style.margin = "2em";
+	underLine.style.margin = "1em";
 	underLine.style.display = "inline-block";
 
 	correct.append(underLine)
 }
 
-console.log(randomize())
-test.addEventListener('keyup', event => {
+console.log(randomWord)
+main.addEventListener('keyup', event => {
 	console.log('Key down: ', event.key)
 
-	let letter = randomize().toLowerCase().split('').filter(element => element == event.key)
-	letter.forEach(element => {
-		// console.log(element)
-		if(element == event.key){
-			console.log('true')
+	let rightLetter = randomWord.toLowerCase().split('').filter(rightElement => rightElement = event.key)
+	let wrongLetter = randomWord.toLowerCase().split('').filter(wrongElement => wrongElement != event.key)
+
+	rightLetter.forEach(rightElement => {
+
+		if(rightElement == event.key){
+			console.log('true');
 		}
-		else{
-			console.log('fel')
-			scaffold.style.display = visible
+	})
+	wrongLetter.forEach(wrongElement => {
+		
+		if(wrongElement != event.key){
+			console.log('nope')
 		}
 	})
 })
