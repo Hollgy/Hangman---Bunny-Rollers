f9920af021331ab40ac6076d146ae484b5e68db3
KG <karlgustav.ek@hotmail.com>
2023-02-07 12:58:21 +0100

    semi functional input of user into array, no display yet

--- user.js
+++ user.js
@@ -7,13 +7,11 @@ const userInput = document.querySelector('.user-input')
 const scoreOverlay = document.querySelector('.overlay')
 const scoreButton = document.querySelector('.open-score')
 const head = document.querySelector('header')
-
-// VARIABLER
 const invisible = 'none';
 const visible = 'block';
 
+// ------------------------------------
 let isVisible = false;
-
 scoreOverlay.style.display = invisible;
 scoreButton.addEventListener('click', () => {
     if (!isVisible) {
@@ -24,13 +22,35 @@ scoreButton.addEventListener('click', () => {
         isVisible = false;
     }
 });
-
+// ------------------------------------
 // stoppar input i username fältet att räknas som gissning
 head.addEventListener("keyup", function (h) {
     h.stopPropagation()
-    console.log("Knapptryck")
+    console.log("input i field")
 })
+// ------------------------------------
+let users = [];
+
+const addUser = (ev)=>{
+    ev.preventDefault()
+    let user = {
+        username: document.getElementById('userInput').value,
+    }
+    users.push(user);
+    // visar en lista
+    console.warn('added', {users} );
+    let pre = document.querySelector('.overlay pre');
+    pre.textContent = '\n' + JSON.stringify(users, '\t', 2)
+    
+    // sparar lokalt
+    localStorage.setItem('UserList', JSON.stringify(users));
+}
+
+document.addEventListener('DOMContentLoaded', () =>{
+    document.getElementById('userBtn').addEventListener('click', addUser)
+});
+// ------------------------------------
 
 
 
-
+// ------------------------------------
